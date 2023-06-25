a5c6587e555a5528a569a3f73caccc9e6da7f10c
JunAng <wangjunang94@gmail.com>
2023-06-25 19:04:26 +0800

    fix inter_xx

--- Graphene_Generator_v4_Gpu.py
+++ Graphene_Generator_v4_Gpu.py
@@ -1,5 +1,8 @@
 import torch
 from math import *
+import numpy as np
+from scipy import integrate
+from numba import jit,cuda,vectorize,float64
 
 
 ###########################################################################
@@ -121,7 +124,7 @@ class Graphene_SS:
         E_p = torch.sqrt(self.epsilon_p**2 + self.Delta**2)
         E_m = torch.sqrt(self.epsilon_m**2 + self.Delta**2) 
         E_0 = torch.sqrt(self.epsilon_0**2 + self.Delta**2)
-        D_inter_xx = ((~masker_mu_neq_0*4*self.Delta**2 *self.state_p_kx_m**2* (  torch.nan_to_num( self.epsilon_0 / self.mu,nan=0 )*(- 1/E_p + 1/E_m )  )).mean(dim=(-2,-1)) /2 - self.inter_step()
+        D_inter_xx = ((~masker_mu_neq_0*self.scipy_integral()) - self.inter_step()
                + (masker_mu_neq_0*4*self.Delta**2 *self.state_p_kx_m**2* 2*self.epsilon_0**2/E_0**3*Occupy_f(E_0,self.B)  ).mean(dim=(-2,-1)) /2
                - self.Integrated_inter_xx(masker_mu_neq_0)      ).cpu()
 
@@ -137,6 +140,55 @@ class Graphene_SS:
         #D_inter_xy = ((4*self.Delta**2 *self.state_p_kx_m*self.state_p_ky_m* ( torch.nan_to_num( ( self.epsilon_0 / self.mu )*(- Occupy_f(self.E_p,self.B)/self.E_p + Occupy_f(self.E_m,self.B)/self.E_m ),nan=0 ) + masker_mu_neq_0* 2*self.epsilon_p**2/self.E_p**3 *(Occupy_f(self.E_p,self.B) - self.E_p*Lorentzian(self.E_p - abs(self.B))) )).mean(dim=(-2,-1)) /2).cpu()
 
         return D_inter_xx,D_inter_yy
+
+    def scipy_integral(self):
+        """Integral inter_xx by using scipy integrate"""
+        @vectorize([float64(float64,float64)])
+        def step(E,B):
+            if E > B:
+                return 1
+            elif E<B:
+                return 0
+            else:
+                return 0.5
+
+        @vectorize([float64(float64,float64,float64,float64,float64)])
+        def f(y,x,t,mu,B):
+            u = np.cos(y)
+            v = np.cos(x)
+            E_p = (np.sqrt((t*np.sqrt(4*(u*(u+v))+1)+1*mu)**2+self.Delta**2))
+            E_m = (np.sqrt((t*np.sqrt(4*(u*(u+v))+1)+-1*mu)**2+self.Delta**2))
+            if 4*(u*(u+v))+1 == 0:
+                I = 0
+            else: 
+                I = t*(-2*u**2+u*v+1)**2/(4*(u*(u+v))+1)**1.5*( -step(E_p,B)/E_p +  step(E_m,B)/E_m )/2/np.pi**2/mu/4
+            return I
+
+        @vectorize([float64(float64,float64,float64)])
+        def Integrate(t,mu,B):
+            I=integrate.dblquad(f,0,2*np.pi,-np.pi,np.pi,args=(t,mu,B))[0]
+            return I
+
+        return Integrate(self.t.cpu(),self.mu.cpu(),self.B.cpu()) 
+        # u = lambda y: np.cos(y)
+        # v = lambda x: np.cos(x)
+        # E = lambda alpha,t,mu,x,y: (np.sqrt((t*np.sqrt(4*(u(y)*(u(y)+v(x)))+1)+alpha*mu)**2+self.Delta**2))
+        # def step(E,B=0):
+        #   return np.heaviside(E,abs(B))
+        # def f(y,x,t,mu,B):
+        #     if 4*(u(y)*(u(y)+v(x)))+1 == 0:
+        #         I = 0
+        #     else: 
+        #         I = t*(-2*u(y)**2+u(y)*v(x)+1)**2/(4*(u(y)*(u(y)+v(x)))+1)**1.5*( -step(E(1,t,mu,x,y),B)/E(1,t,mu,x,y) +  step(E(-1,t,mu,x,y),B)/E(-1,t,mu,x,y) )/2/np.pi**2/mu/4
+        #     return I
+        
+        # I = torch.zeros(self.t.shape[0],self.mu.shape[1],self.mu.shape[2],self.B.shape[3])
+        # for i in self.t.shape[0]:
+        #     for j in self.mu.shape[1]:
+        #         for m in self.mu.shape[2]:
+        #             for n in self.B.shape[3]:
+        #                 I[i,j,m,n]=integrate.dblquad(f,0,2*np.pi,-np.pi,np.pi,args=(self.t[i].item(),self.mu[0,j,m].item(),self.B[0,0,0,n].item()))[0]
+        # return I
     
     def inter_step(self):
         u = torch.cos(torch.linspace(-torch.pi,0,1500)).reshape(1,1,1,1,-1,1).to('cuda')
